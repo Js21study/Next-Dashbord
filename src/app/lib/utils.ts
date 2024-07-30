aeb3d587133bce58f372b439c1f605a12f506882
student@student.com <ti@admin.com>
2024-07-30 11:46:15 +0300

    added connection to db and models for user and product

---
+++ src/app/lib/utils.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+interface IConnection {
+  isConnected?: boolean;
+}
+
+export const connectToDB = async (): Promise<void> => {
+  const connection: IConnection = {};
+
+  try {
+    if (connection.isConnected) return;
+    const db: any = await mongoose.connect(process.env.MONGO_DB as string);
+    connection.isConnected = db.connection[0].readyState;
+  } catch (error) {
+    console.log(error);
+  }
+};
